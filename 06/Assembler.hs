6cf84b152e714447634559d450a3f25b8acceabc
hsine <extraordinary1022@gmail.com>
2016-03-23 00:10:23 +0800

    can translate A instrunction

--- 06/Assembler.hs
+++ 06/Assembler.hs
@@ -1,11 +1,21 @@
+module Main where
+
 import System.Environment(getArgs)
 import Data.Char(isSpace)
+import Numeric(showIntAtBase)
+import Sub
+
+--translate A instruction
+instruction ('@':xs) = '0':bin15 (read xs::Int)
+--translate C instruction
+instruction xs = xs
+
 
 translate :: [String] -> [String]
 translate [] = []
 translate (l:ls) = case filter (not.isSpace) l of
 	('/':'/':xs) -> translate ls
-	xs -> xs:translate ls
+	xs -> instruction xs:translate ls
 
 main = do
 	fileName <- fmap head getArgs
