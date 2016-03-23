01b44630f60ab9b26ea27b80a9bd1d20aab3657b
hsine <extraordinary1022@gmail.com>
2016-03-23 23:03:27 +0800

    it's working, 'looks' and 'acts' fine

--- 06/Assembler.hs
+++ 06/Assembler.hs
@@ -3,24 +3,25 @@ import Data.Char(isSpace,isAlpha)
 import Data.List(isPrefixOf)
 import qualified Data.Map as M
 import Numeric(showIntAtBase)
-import Decode
-import SymTab
+import Decode(decode)
+import SymTab(buildSymTab)
 
 despaces = filter (not.(=="")) . map trimLine where
-    trimLine = (reverse.(dropWhile isSpace).reverse.(dropWhile isSpace))
+    trimLine = reverse.(dropWhile isSpace).reverse.(dropWhile isSpace)    -- trim both ends
 
 decomments ls = filter (not.isPrefixOf "//") ls
 
 decodes ls = map decode ls
 
-desymbols ls = map desym ls where
+desymbols ls = map desym (delabels ls) where
     symtab = buildSymTab ls
+    delabels = filter ((/='(').head)
     desym l@('@':sym)
         | isAlpha (head sym) = case M.lookup sym symtab of
             Just value -> "@" ++ show value
             Nothing -> error "Symbol not found"
-        | otherwise = l
-    desym l = l
+        | otherwise = l -- A instrunction in literal
+    desym l = l -- not A instrunction, leave it as it is
 
 main = do
     fileName <- fmap head getArgs
--- 06/SymTab.hs
+++ 06/SymTab.hs
@@ -1,7 +1,10 @@
 module SymTab (
     buildSymTab
 )where
+
 import qualified Data.Map as M
+import Data.Char(isAlpha)
+import Data.Maybe(isNothing)
 
 hackBuiltInSymbol = M.fromList
     [ ("SP",0), ("LCL",1), ("ARG",2), ("THIS",3), ("THAT",4)
@@ -10,4 +13,27 @@ hackBuiltInSymbol = M.fromList
     , ("R12",12), ("R13",13), ("R14",14), ("R15",15)
     , ("SCREEN",16384), ("KBD",24576) ]
 
-buildSymTab _ = hackBuiltInSymbol
+--on seeing variable
+--    lookup table
+--        exist -> skip
+--        nope -> assume it's new variable
+--on seeing label
+--    overwrite to table
+
+-- varibles defined to be start from 16
+buildSymTab ls = _buildSymTab ls 0 16 hackBuiltInSymbol where
+    _buildSymTab [] _ _ symtab = symtab
+    _buildSymTab (l:ll) lineCount varCount symtab = case l of
+        --it's a new label
+        '(':xs ->  _buildSymTab ll lineCount varCount insertedTab where
+            label = init xs -- take out ')'
+            insertedTab = M.insert label lineCount symtab
+        --might be a new variable
+        '@':xs -> if isVariable && notInTable then insertedTab else moreSymTab where
+            isVariable = isAlpha (head xs)
+            notInTable = isNothing (M.lookup xs symtab)
+            insertedTab = _buildSymTab ll (lineCount+1) (varCount+1) (M.insert xs varCount symtab)
+            moreSymTab = _buildSymTab ll (lineCount+1) (varCount) symtab
+        otherwise -> _buildSymTab ll (lineCount+1) varCount symtab
+
+main = putStrLn "sym"
