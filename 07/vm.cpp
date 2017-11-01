262c0db0f68752686d04097881d536fda8fa8a29
hsinewu <extraordinary1022@gmail.com>
2017-11-01 13:12:53 +0800

    imp operators, StackTest tested

--- 07/vm.cpp
+++ 07/vm.cpp
@@ -1,8 +1,11 @@
 #include <sstream>
 #include <map>
+#include <stdexcept>
 using namespace std;
 
 using F = string (*)(string);
+static int uniq_num = 0;
+
 string load_constant(string val) {
 
 	return "@" + val + "\nD=A\n";
@@ -26,11 +29,68 @@ string command_push(string seg, string val) {
 	return funcs[seg](val) + write_stack() + inc_stack();
 }
 
+// Arithmetics
 string command_add() {
 
-	static string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
-	static string op2_add = "@SP\nA=M-1\nM=M+D\n";
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_add = "@SP\nA=M-1\nM=M+D\n";
 	return op1 + op2_add;
+}
+
+string command_sub() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_sub = "@SP\nA=M-1\nM=M-D\n";
+	return op1 + op2_sub;
+}
+
+string command_neg() {
+	return "@SP\nA=M-1\nM=-M";
+}
+
+// Comparision
+string command_eq() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_sub = "@SP\nA=M-1\nM=M-D\n";
+	// M = M==0? 1: 0
+	const string label = "VM_EQ_" + to_string(++uniq_num);
+	const string test = "D=M\nM=-1\n"  // Guess true
+		"@" + label + "\nD;JEQ\n"     // Jump/skip if correct
+		"@SP\nA=M-1\nM=0\n"                  // Set false
+		"(" + label + ")\n";          // Label for exit
+	return op1 + op2_sub + test;
+}
+
+string command_gt() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_sub = "@SP\nA=M-1\nM=M-D\n";
+	const string label = "VM_GT_" + to_string(++uniq_num);
+	const string test = "D=M\nM=-1\n@" + label + "\nD;JGT\n@SP\nA=M-1\nM=0\n(" + label + ")\n";
+	return op1 + op2_sub + test;
+}
+
+string command_lt() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_sub = "@SP\nA=M-1\nM=M-D\n";
+	const string label = "VM_LT_" + to_string(++uniq_num);
+	const string test = "D=M\nM=-1\n@" + label + "\nD;JLT\n@SP\nA=M-1\nM=0\n(" + label + ")\n";
+	return op1 + op2_sub + test;
+}
+
+// Bit-wise
+string command_and() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_and = "@SP\nA=M-1\nM=M&D\n";
+	return op1 + op2_and;
+}
+
+string command_or() {
+	const string op1 = "@SP\nM=M-1\nA=M\nD=M\n";
+	const string op2_or = "@SP\nA=M-1\nM=M|D\n";
+	return op1 + op2_or;
+}
+
+string command_not() {
+	return "@SP\nA=M-1\nM=!M";
 }
 
 string parse_line(string line) {
@@ -42,5 +102,21 @@ string parse_line(string line) {
 		return command_push(seg, val);
 	if( cmd == "add")
 		return command_add();
-	return "Command not implemented: [" + cmd + "]";
+	if( cmd == "sub")
+		return command_sub();
+	if( cmd == "neg")
+		return command_neg();
+	if( cmd == "eq")
+		return command_eq();
+	if( cmd == "gt")
+		return command_gt();
+	if( cmd == "lt")
+		return command_lt();
+	if( cmd == "and")
+		return command_and();
+	if( cmd == "or")
+		return command_or();
+	if( cmd == "not")
+		return command_not();
+	throw runtime_error("Unknown command [" + cmd + "]");
 }
