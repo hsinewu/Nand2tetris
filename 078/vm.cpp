c5f8db0f4756bf1271d44aa03a9a847af5dfd911
hsinewu <extraordinary1022@gmail.com>
2017-11-20 15:51:25 +0800

    *vm.cpp

--- 078/vm.cpp
+++ 078/vm.cpp
@@ -3,80 +3,79 @@
 #include <stdexcept>
 using namespace std;
 
-static int uniq_num = 0;
 extern string G_FILE_BASENAME;
 
 // D = val
-string load_constant(string val) {
+string load_addr(string val) {
 
 	return "@" + val + "\nD=A\n";
 }
 
+// D = M
+string load_memory(string var) {
+
+	return "@" + var + "\nD=M\n";
+}
+
 // D = PTR
-string load_address(string seg, string val) {
+string load_segment(string seg, string val) {
 
 	const map< string, string> shorthand = {
-		{"local", "@LCL\n"},
-		{"argument", "@ARG\n"},
-		{"this", "@THIS\n"},
-		{"that", "@THAT\n"}
+		{"local", "LCL"},
+		{"argument", "ARG"},
+		{"this", "THIS"},
+		{"that", "THAT"}
 	};
 
 	// TEMP is defined as @5 ~ @12
 	if( seg == "temp")
-		return load_constant( to_string( 5 + stoi(val)));
+		return load_addr( to_string( 5 + stoi(val)));
 
 	// pointer 0/1 = THIS/THAT
 	if( seg == "pointer")
-		return load_constant( to_string( 3 + stoi(val)));
+		return load_addr( to_string( 3 + stoi(val)));
 
 	// static i = @Foo.i where Foo is the name of vm file
 	if( seg == "static")
-		return load_constant(val) + "@" + G_FILE_BASENAME + "." + val + "\nD=M+D\n";
+		return load_addr( G_FILE_BASENAME + "." + val);
 
 	if( shorthand.count(seg) == 0 )
 		throw runtime_error( "Bad seg name: " + seg );
 
 	// optimize for val == 0
 	if( val == "0")
-		return shorthand.at(seg) + "D=M\n";
+		return load_memory( shorthand.at(seg));
 
-	return load_constant(val) + shorthand.at(seg) + "D=M+D\n";
+	return load_addr(val) + "@" + shorthand.at(seg) + "\nD=M+D\n";
 }
 
-// SP++
-string inc_stack() {
+// *SP++ = D
+string push_stack() {
 
-	return "@SP\nM=M+1\n";
+	return "@SP\nM=M+1\nA=M-1\nM=D\n";
 }
 
-// SP--
-string dec_stack() {
+// A = --SP
+string pop_stack() {
 
-	return "@SP\nM=M-1\n";
-}
-
-// *SP = D
-string write_stack() {
-
-	return "@SP\nA=M\nM=D\n";
+	return "@SP\nM=M-1\nA=M\n";
 }
 
 string command_push(string seg, string val) {
 
 	if( seg == "constant")
-		return load_constant(val) + write_stack() + inc_stack();
+		return load_addr(val) + push_stack();
 
-	return load_address( seg, val) + "A=D\nD=M\n" + write_stack() + inc_stack();
+	return load_segment( seg, val) + "A=D\nD=M\n" + push_stack();
 }
 
 string command_pop(string seg, string val) {
 
-	const string load_stack = dec_stack() + "A=M\n" "A=M\n";
+	const string pop_stackA = pop_stack() + "A=M\n";
 	const string swap_AD = "D=D+A\nA=D-A\nD=D-A\n";
 	const string write_back = "M=D\n";
 
-	return load_address( seg, val) + load_stack + swap_AD + write_back;
+	return load_segment( seg, val) + pop_stackA + swap_AD + write_back;
 }
 
 // D = op2, M = op1 ( A = SP )
@@ -84,65 +83,46 @@ const string set_ops = "@SP\nM=M-1\nA=M\nD=M\n@SP\nA=M-1\n";
 const string set_op = "@SP\nA=M-1\n";  // M = op ( A = SP )
 
 // Arithmetics
-string command_add() {
-
-	return set_ops + "M=M+D\n";
-}
-
-string command_sub() {
-
-	return set_ops + "M=M-D\n";
-}
-
-string command_neg() {
-
-	return set_op + "M=-M\n";
-}
+const string command_add = set_ops + "M=M+D\n";
+const string command_sub = set_ops + "M=M-D\n";
+const string command_neg = set_op + "M=-M\n";
 
 // Comparision
+string uniq_label() {
+	static int uniq_num;
+	return "VM_" + to_string( ++uniq_num);
+}
+
+string test(string jump) {
+
+	const string label = uniq_label();
+	const string set_bits = "D=M\nM=-1\n";
+	const string jump_exit_if = "@" + label + "\nD;" + jump + "\n";
+	const string unset_bits = "@SP\nA=M-1\nM=0\n";
+	const string exit_label = "(" + label + ")\n";
+
+	return set_bits + jump_exit_if + unset_bits + exit_label;
+}
+
 string command_eq() {
 
-	// M = M==0? -1: 0
-	const string label = "VM_EQ_" + to_string( ++uniq_num);
-	const string test = "D=M\nM=-1\n"  // Guess true
-		"@" + label + "\nD;JEQ\n"      // Jump/skip if correct
-		"@SP\nA=M-1\nM=0\n"            // Set false
-		"(" + label + ")\n";           // Label for exit
-
-	return command_sub() + test;
+	return command_sub + test("JEQ");
 }
 
 string command_gt() {
 
-	const string label = "VM_GT_" + to_string( ++uniq_num);
-	const string test = "D=M\nM=-1\n@" + label + "\nD;JGT\n@SP\nA=M-1\nM=0\n(" + label + ")\n";
-
-	return command_sub() + test;
+	return command_sub + test("JGT");
 }
 
 string command_lt() {
 
-	const string label = "VM_LT_" + to_string( ++uniq_num);
-	const string test = "D=M\nM=-1\n@" + label + "\nD;JLT\n@SP\nA=M-1\nM=0\n(" + label + ")\n";
-
-	return command_sub() + test;
+	return command_sub + test("JLT");
 }
 
 // Bit-wise
-string command_and() {
-
-	return set_ops + "M=M&D\n";
-}
-
-string command_or() {
-
-	return set_ops + "M=M|D\n";
-}
-
-string command_not() {
-
-	return set_op + "M=!M\n";
-}
+const string command_and = set_ops + "M=M&D\n";
+const string command_or  = set_ops + "M=M|D\n";
+const string command_not = set_op + "M=!M\n";
 
 // Jump
 string command_label(string label) {
@@ -168,7 +148,7 @@ string command_function(string name, string nvars) {
 
 	// This makes code grows n times
 	// Better use jump?
-	string pad_local = "@SP\nM=M+1\nA=M-1\nM=0\n";
+	const string pad_local = "@SP\nM=M+1\nA=M-1\nM=0\n";
 	string pad_locals = "";
 	for( int n = stoi(nvars); n>0; n--)
 		pad_locals += pad_local;
@@ -177,26 +157,9 @@ string command_function(string name, string nvars) {
 	// label this function address
 	command_label(name) + 
 
-	// store return address
-	// IP ???
-
-	// store pointers
-	// *sp++ = ptr for ptr in [LCL, ARG, THIS, THAT]
-	//  "@LCL\nD=M\n" + write_stack() + inc_stack() +
-	//  "@ARG\nD=M\n" + write_stack() + inc_stack() +
-	// "@THIS\nD=M\n" + write_stack() + inc_stack() +
-	// "@THAT\nD=M\n" + write_stack() + inc_stack() +
-	
-	// set local arg
-	// lcl = sp
-	// "@SP\nD=M\n" "@LCL\nM=D\n"
-
 	// reserve n vars
 	// zero initialize
 	pad_locals 
-
-	// course says push 0, but why?
-	// "@SP\nM=M+1\nA=M-1\nM=0\n"
 	;
 }
 
@@ -243,17 +206,23 @@ string parse_line(string line) {
 	string cmd, a, b;
 	tokens >> cmd >> a >> b;
 
+	// commands that always generate same string
+	const static map<string, string> static_cmd = {
+		{"add", command_add},
+		{"sub", command_sub},
+		{"neg", command_neg},
+		{"and", command_and},
+		{"or" , command_or },
+		{"not", command_not},
+	};
+	if( static_cmd.count( cmd))
+		return static_cmd.at(cmd);
+
+	// commands that don't always generate same string
 	if( cmd == "push")
 		return command_push(a, b);
 	if( cmd == "pop")
 		return command_pop(a, b);
-
-	if( cmd == "add")
-		return command_add();
-	if( cmd == "sub")
-		return command_sub();
-	if( cmd == "neg")
-		return command_neg();
 
 	if( cmd == "eq")
 		return command_eq();
@@ -261,13 +230,6 @@ string parse_line(string line) {
 		return command_gt();
 	if( cmd == "lt")
 		return command_lt();
-
-	if( cmd == "and")
-		return command_and();
-	if( cmd == "or")
-		return command_or();
-	if( cmd == "not")
-		return command_not();
 
 	if( cmd == "label")
 		return command_label(a);
